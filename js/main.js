7fd5d1214b02f8d5eb0c5a5a6d699da87c080905
Murilo Dorigo <murillodorigo@gmail.com>
2023-06-23 20:12:52 -0300

    adicionando um form no html e começando a lógica no javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-const input = document.getElementById("task")
+const todoForm = document.getElementById("form")
+
+todoForm.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    const task = event.target.elements['task']
+    console.log(task.value) 
+})
+
+function addNewTask(){
+    const newTask = document.createElement('li')
+    newTask.classList.add("item")
+    
+}
